1211df704a5f680af93a5c81e7f585a835fdab2d
gqd000 <2826407926@qq.com>
2022-06-26 17:36:32 +0800

    添加部分后台接口

---
+++ src/main/java/com/digging/controller/admin/CategoryList.java
@@ -0,0 +1,11 @@
+package com.digging.controller.admin;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping
+public class CategoryList {
+}
